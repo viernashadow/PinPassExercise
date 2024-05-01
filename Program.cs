e586f48e89d8cd0baad9486e1e19d3412de5c54c
Galaxia <viernashadow@gmail.com>
2024-05-01 15:40:13 -0400

    You can now enter a pin with unlimited tries until you get it right

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
 ﻿
 Console.WriteLine("Welcome to the bank of C#!");
 
+int pin = 4980;
+int userEntry;
+
+Console.WriteLine("Please enter your Pin to proceed");
+
+userEntry = int.Parse(Console.ReadLine());
+
+while (userEntry != pin)
+{
+    Console.WriteLine("Incorrect Pin, try again {tries remaining}");
+    userEntry = int.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine("Pin accepted!");
 
