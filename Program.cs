281bec3fb0f2b3b97b5bff9b619f7b9aca298b62
Galaxia <viernashadow@gmail.com>
2024-05-04 18:56:02 -0400

    Added trycatches to the pin

--- Program.cs
+++ Program.cs
@@ -3,16 +3,30 @@ Console.WriteLine("Welcome to the bank of C#!");
 
 int pin = 4980;
 int userEntry;
+userEntry = pin + 5;
 
 Console.WriteLine("Please enter your Pin to proceed");
 
-userEntry = int.Parse(Console.ReadLine());
+try
+{
+    userEntry = int.Parse(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("That's definitely not your pin because it's not a number, try again!!");
+}
 
 while (userEntry != pin)
 {
     Console.WriteLine("Incorrect Pin, try again {tries remaining}");
-    userEntry = int.Parse(Console.ReadLine());
-
+    try
+    {
+        userEntry = int.Parse(Console.ReadLine());
+    }
+    catch
+    {
+        Console.WriteLine("Woah that can't be your pin, it's not a number! Try again!");
+    }
 }
 
 Console.WriteLine("Pin accepted!");
