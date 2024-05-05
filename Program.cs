b71879cca35137f03215819ee598bf03aeb14906
Galaxia <viernashadow@gmail.com>
2024-05-04 20:47:18 -0400

    Made triesRemaining work and fixed pin lockout

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ int pin = 4980;
 int userEntry;
 userEntry = pin + 5; // instead of initializing it as zero it's pin plus five that way it can never be the users pin if they choose it
 int pinLockout = 0;
+int triesRemaining = 3;
 
 Console.WriteLine("Hey, what's the Password?");
 pp = Console.ReadLine();
@@ -23,27 +24,36 @@ Console.WriteLine("Please enter your Pin to proceed:");
 try
 {
     userEntry = int.Parse(Console.ReadLine());
+    triesRemaining--;
 }
 catch
 {
     Console.WriteLine("That's definitely not your pin because it's not a number, try again!");
+    triesRemaining--;
+
 
 }
 
 while (userEntry != pin)
 {
-    if (pinLockout != 3)
+    if (pinLockout <= 3)
     {
-        Console.WriteLine("Incorrect Pin, try again {tries remaining}");
+        Console.WriteLine($"Incorrect Pin, try again.. you have {triesRemaining} tries remaining");
         pinLockout++;
         try
         {
             userEntry = int.Parse(Console.ReadLine());
+            pinLockout++;
+            triesRemaining--;
+            continue;
+
         }
         catch
         {
             Console.WriteLine("Woah that can't be your pin, it's not a number! Try again!");
             pinLockout++;
+            triesRemaining--;
+
 
         }
     }
